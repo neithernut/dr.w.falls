cd17200c26fb0a929ec1d5cafd87a8692b6c77a2
Julian Ganz <neither@nut.email>
2021-01-12 22:09:16 +0100

    gameplay: add type for field of settled elements

--- src/gameplay.rs
+++ src/gameplay.rs
@@ -1,7 +1,54 @@
 //! Gameplay related types, functions and utilities
 
+use std::ops;
+
+
 const FIELD_WIDTH: usize = 8;
 const FIELD_HEIGHT: usize = 16;
+
+
+/// Representation of a field of settled/non-moving elements
+///
+/// The game field has 16 rows and 8 columns. The top-level index refers to a
+/// row, with `0` referring to the top row. The second-level index refers to the
+/// column, with `0` referring to the left-most column.
+///
+/// Only rows `1` through `15` are accessible on this field. Any attempt to
+/// access the top row will result in a panic.
+///
+#[derive(Default)]
+pub struct StaticField {
+    rows: [[Tile; FIELD_WIDTH]; FIELD_HEIGHT - 1],
+}
+
+impl ops::IndexMut<usize> for StaticField {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.rows[index + 1]
+    }
+}
+
+impl ops::Index<usize> for StaticField {
+    type Output = [Tile; FIELD_WIDTH];
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.rows[index + 1]
+    }
+}
+
+
+/// Representation of a tile
+///
+pub enum Tile {
+    None,
+    CapsuleElement(CapsuleElement),
+    Virus(Virus),
+}
+
+impl Default for Tile {
+    fn default() -> Self {
+        Self::None
+    }
+}
 
 
 /// Representation of a capsule

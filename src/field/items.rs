aa6a1b627f52abadfb45177fc15bcf58d6205cd5
Julian Ganz <neither@nut.email>
2021-01-17 17:37:02 +0100

    field: add representation of a virus

--- src/field/items.rs
+++ src/field/items.rs
@@ -1,2 +1,25 @@
 //! Types representing items occupying individual tiles
 
+use crate::util::Colour;
+
+
+/// Representation of a virus
+///
+pub struct Virus {
+    colour: Colour,
+}
+
+impl Virus {
+    /// Create a new virus with the given colour
+    ///
+    pub fn new(colour: Colour) -> Self {
+        Self {colour}
+    }
+
+    /// Retrieve the virus' colour
+    ///
+    pub fn colour(&self) -> Colour {
+        self.colour
+    }
+}
+

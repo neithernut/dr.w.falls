c3c31f3d8a7de6effc2365b60e352e541dbc34a9
Julian Ganz <neither@nut.email>
2021-01-17 20:49:52 +0100

    gameplay: add type representing a static field

--- src/gameplay/static_field.rs
+++ src/gameplay/static_field.rs
@@ -1,2 +1,62 @@
 //! Definition of the static field and associated types
 
+use crate::util;
+
+use super::items::{CapsuleElement, Virus};
+use super::row::Row;
+
+
+/// Field of settled/non-moving elements
+///
+#[derive(Default)]
+pub struct StaticField {
+    data: [Row<TileContents>; util::FIELD_HEIGHT as usize],
+}
+
+impl std::ops::IndexMut<util::Position> for StaticField {
+    fn index_mut(&mut self, index: util::Position) -> &mut Self::Output {
+        &mut self.data[usize::from(index.0)][index.1]
+    }
+}
+
+impl std::ops::Index<util::Position> for StaticField {
+    type Output = TileContents;
+
+    fn index(&self, index: util::Position) -> &Self::Output {
+        &self.data[usize::from(index.0)][index.1]
+    }
+}
+
+
+/// Representation of a single tile's contents
+///
+pub enum TileContents {
+    None,
+    CapsuleElement(CapsuleElement),
+    Virus(Virus),
+}
+
+impl TileContents {
+    /// Take the tile's contents, leaving it unoccupied
+    ///
+    pub fn take(&mut self) -> Self {
+        std::mem::take(self)
+    }
+}
+
+impl Default for TileContents {
+    fn default() -> Self {
+        Self::None
+    }
+}
+
+impl util::PotentiallyColoured for TileContents {
+    fn colour(&self) -> Option<util::Colour> {
+        match self {
+            Self::None => None,
+            Self::CapsuleElement(e) => Some(e.colour()),
+            Self::Virus(v) => Some(v.colour()),
+        }
+    }
+}
+

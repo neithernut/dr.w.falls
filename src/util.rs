95ba23c984159d38ff4eaf27b68661a1ab3b31ef
Julian Ganz <neither@nut.email>
2021-01-17 22:04:31 +0100

    util: add ranges covering all rows or columns as constants
    We'll often have to iterate over all rows or columns.

--- src/util.rs
+++ src/util.rs
@@ -77,6 +77,11 @@ impl TryFrom<usize> for RowIndex {
 }
 
 
+/// Range including all rows
+///
+pub const ROWS: RangeInclusive<RowIndex> = RangeInclusive::new(RowIndex::TOP_ROW, RowIndex::BOTTOM_ROW);
+
+
 /// Column index type
 ///
 /// Instances of this type serve as an index for a column in a field. It
@@ -110,6 +115,14 @@ impl TryFrom<usize> for ColumnIndex {
         value.try_into().ok().filter(|i| *i < FIELD_WIDTH).map(|data| Self {data}).ok_or(value)
     }
 }
+
+
+/// Range including all columns
+///
+pub const COLUMNS: RangeInclusive<ColumnIndex> = RangeInclusive::new(
+    ColumnIndex::LEFTMOST_COLUMN,
+    ColumnIndex::RIGHTMOST_COLUMN
+);
 
 
 /// Project-specific partial predefinition of `std::iter::Step`

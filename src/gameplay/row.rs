a8c4c4c63f7cf2f71955fec24fa1d8c40299d5e7
Julian Ganz <neither@nut.email>
2021-01-17 20:28:18 +0100

    gameplay: add module internal row utility container

--- src/gameplay/row.rs
+++ src/gameplay/row.rs
@@ -1,2 +1,35 @@
 //! Definition of a type representing a signle row
 
+use crate::util;
+
+
+/// A single row within a field
+///
+/// This type is indended as an internal convenience utility for construcing
+/// fields.
+///
+#[derive(Default)]
+pub struct Row<T>
+    where T: Default
+{
+    data: [T; util::FIELD_WIDTH as usize],
+}
+
+impl<T> std::ops::IndexMut<util::ColumnIndex> for Row<T>
+    where T: Default
+{
+    fn index_mut(&mut self, index: util::ColumnIndex) -> &mut Self::Output {
+        &mut self.data[usize::from(index)]
+    }
+}
+
+impl<T> std::ops::Index<util::ColumnIndex> for Row<T>
+    where T: Default
+{
+    type Output = T;
+
+    fn index(&self, index: util::ColumnIndex) -> &Self::Output {
+        &self.data[usize::from(index)]
+    }
+}
+

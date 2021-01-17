854bc06954999783e3264e2562d1d1984fa350f4
Julian Ganz <neither@nut.email>
2021-01-17 17:46:08 +0100

    gameplay: add type representing a capsule element

--- src/gameplay/items.rs
+++ src/gameplay/items.rs
@@ -1,6 +1,6 @@
 //! Types representing items occupying individual tiles
 
-use crate::util::Colour;
+use crate::util::{Colour, Direction};
 
 
 /// Representation of a virus
@@ -17,6 +17,38 @@ impl Virus {
     }
 
     /// Retrieve the virus' colour
+    ///
+    pub fn colour(&self) -> Colour {
+        self.colour
+    }
+}
+
+
+/// Representation of a capsule element
+///
+pub struct CapsuleElement {
+    colour: Colour,
+    /// Direction of any capsule element bound to this one
+    ///
+    pub partner: Option<Direction>,
+}
+
+impl CapsuleElement {
+    /// Create a new capsule element
+    ///
+    pub fn new(colour: Colour, partner: Option<Direction>) -> Self {
+        Self {colour, partner}
+    }
+
+    /// Create a new unbound capsule element
+    ///
+    /// The capsule element will not have a partner.
+    ///
+    pub fn new_single(colour: Colour) -> Self {
+        Self::new(colour, None)
+    }
+
+    /// Retrieve the capsule element's colour
     ///
     pub fn colour(&self) -> Colour {
         self.colour

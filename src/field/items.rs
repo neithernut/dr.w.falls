53e00ae271da9c8dcfcd40232d78f65deed990b7
Julian Ganz <neither@nut.email>
2021-01-17 17:46:08 +0100

    field: add type representing a capsule element

--- src/field/items.rs
+++ src/field/items.rs
@@ -1,6 +1,7 @@
 //! Types representing items occupying individual tiles
 
-use crate::util::Colour;
+use crate::util;
+use util::{Colour, Direction};
 
 
 /// Representation of a virus
@@ -20,6 +21,44 @@ impl Virus {
     ///
     pub fn colour(&self) -> Colour {
         self.colour
+    }
+}
+
+
+/// Representation of a capsule element
+///
+pub struct CapsuleElement {
+    colour: Colour,
+    /// Direction of any capsule element bound to this one
+    ///
+    pub partner: Option<Direction>,
+}
+
+impl CapsuleElement {
+    /// Create a new capsule element
+    ///
+    pub fn new(colour: Colour, partner: Option<Direction>) -> Self {
+        Self {colour, partner}
+    }
+
+    /// Create a new unbound capsule element
+    ///
+    /// The capsule element will not have a partner.
+    ///
+    pub fn new_single(colour: Colour) -> Self {
+        Self::new(colour, None)
+    }
+
+    /// Retrieve the capsule element's colour
+    ///
+    pub fn colour(&self) -> Colour {
+        self.colour
+    }
+}
+
+impl util::PotentiallyColoured for Option<CapsuleElement> {
+    fn colour(&self) -> Option<Colour> {
+        self.as_ref().map(|e| e.colour())
     }
 }
 

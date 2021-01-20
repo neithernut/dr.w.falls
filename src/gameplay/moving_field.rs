6fff8fb2201c18c531a369393916e2a225a862ed
Julian Ganz <neither@nut.email>
2021-01-20 19:05:47 +0100

    gameplay: add index type trackign a moving row

--- src/gameplay/moving_field.rs
+++ src/gameplay/moving_field.rs
@@ -21,6 +21,22 @@ impl MovingField {
         self.offset = self.offset.checked_sub(1).unwrap_or(self.data.len() - 1)
     }
 
+    /// Crate a MovingRowIndex for a given mapped row
+    ///
+    pub fn moving_row_index(&self, row: util::RowIndex) -> MovingRowIndex {
+        MovingRowIndex {row: self.transform(row)}
+    }
+
+    /// Convert a MovingRowIndex back to a RowIndex
+    ///
+    pub fn row_index_from_moving(&self, index: MovingRowIndex) -> util::RowIndex {
+        use std::convert::TryInto;
+
+        ((index.row + self.data.len() - self.offset) % self.data.len())
+            .try_into()
+            .expect("Failed to tranfform MovingRowIndex to plain util::RowIndex")
+    }
+
     /// Transform a `RowIndex` to the row's index in the internal array
     ///
     fn transform(&self, row: util::RowIndex) -> usize {
@@ -40,5 +56,17 @@ impl std::ops::Index<util::Position> for MovingField {
     fn index(&self, index: util::Position) -> &Self::Output {
         &self.data[self.transform(index.0)][index.1]
     }
+}
+
+
+/// Index for a moving row
+///
+/// Indexes of this kind refer to one row in the field of moving rows. The row
+/// will be tracked across invocations of `MovingField::tick`, i.e. as it moves
+/// down the field.
+///
+#[derive(Copy, Clone, PartialEq, Eq)]
+pub struct MovingRowIndex {
+    row: usize,
 }
 

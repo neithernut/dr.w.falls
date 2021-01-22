1ba1b7d557711119d7468e0063878f5eca4c834e
Julian Ganz <neither@nut.email>
2021-01-22 18:33:54 +0100

    gameplay: re-export types and functions used by other modules

--- src/gameplay.rs
+++ src/gameplay.rs
@@ -8,3 +8,11 @@ mod row;
 mod static_field;
 mod tick;
 
+
+pub use items::Update;
+pub use static_field::StaticField;
+pub use moving_field::MovingField;
+pub use tick::{settle_elements, eliminate_elements, unsettle_elements};
+pub use movement::{Movement, ControlledCapsule};
+pub use preparation::prepare_field;
+

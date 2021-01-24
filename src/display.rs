eb85e7b988ac4d3e7cc7ac5c1484a32840aa0679
Julian Ganz <neither@nut.email>
2021-01-24 17:12:37 +0100

    display: add types for representing draw commands

--- src/display.rs
+++ src/display.rs
@@ -1,2 +1,149 @@
 //! Display rendering utilities
 
+
+/// Representation of a draw command
+///
+#[derive(Copy, Clone)]
+enum DrawCommand<'s> {
+    /// Clear the entire screen
+    ClearScreen,
+    /// Set the cursor's position
+    ///
+    /// The first component denotes the row, the second one the column.
+    ///
+    SetPos(u16, u16),
+    /// Select Graphic Rendition
+    Format(SGR),
+    /// Put text on the screen at the current cursor position
+    Text(&'s str),
+}
+
+impl DrawCommand<'_> {
+    /// Write the draw commands as an ASNI escape sequence
+    ///
+    fn write_as_ansi(&self, out: &mut impl bytes::BufMut) {
+        match self {
+            DrawCommand::ClearScreen    => out.put_slice(b"\x1b[2J"),
+            DrawCommand::SetPos(r, c)   => out.put_slice(format!("\x1b[{};{}H", r, c).as_bytes()),
+            DrawCommand::Format(param)  => out.put_slice(format!("\x1b[{}m", param.code()).as_bytes()),
+            DrawCommand::Text(s)        => out.put_slice(s.as_bytes()),
+        }
+    }
+}
+
+
+/// Representation of some selected "Select Graphic Rendition" parameters
+///
+#[derive(Copy, Clone)]
+enum SGR {
+    /// Reset to default formatting
+    Reset,
+    /// Change intensity
+    ///
+    /// A value of `None` will reset the intensity to the default.
+    ///
+    Intensity(Option<Intensity>),
+    /// Control underline
+    Underline(bool),
+    /// Control blink
+    Blink(bool),
+    /// Control strike-through/cross-out
+    Strike(bool),
+    /// Set the foreground colour
+    ///
+    /// A value of `None` will reset the colour to the default.
+    ///
+    FGColour(Option<(Colour, Brightness)>),
+    /// Set the background colour
+    ///
+    /// A value of `None` will reset the colour to the default.
+    ///
+    BGColour(Option<(Colour, Brightness)>),
+}
+
+impl SGR {
+    /// Determine the code number for the SGR parameter
+    ///
+    fn code(&self) -> u8 {
+        use Intensity as Int;
+
+        match self {
+            Self::Reset                       =>  0,
+            Self::Intensity(Some(Int::Bold))  =>  1,
+            Self::Intensity(Some(Int::Faint)) =>  2,
+            Self::Intensity(None)             => 22,
+            Self::Underline(true)             =>  4,
+            Self::Underline(false)            => 24,
+            Self::Blink(true)                 =>  5,
+            Self::Blink(false)                => 25,
+            Self::Strike(true)                =>  9,
+            Self::Strike(false)               => 29,
+            Self::FGColour(Some((col, br)))   => 30 + col.code_off() + br.code_off(),
+            Self::FGColour(None)              => 39,
+            Self::BGColour(Some((col, br)))   => 40 + col.code_off() + br.code_off(),
+            Self::BGColour(None)              => 49,
+        }
+    }
+}
+
+
+/// Representation of intensity
+///
+#[derive(Copy, Clone)]
+enum Intensity {
+    Bold,
+    Faint,
+}
+
+
+/// Representation of the basic colour supported by terminals
+///
+#[derive(Copy, Clone)]
+enum Colour {
+    Black,
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    White,
+}
+
+impl Colour {
+    /// Determin the code offset corresponding to the colour
+    ///
+    fn code_off(&self) -> u8 {
+        match self {
+            Self::Black   => 0,
+            Self::Red     => 1,
+            Self::Green   => 2,
+            Self::Yellow  => 3,
+            Self::Blue    => 4,
+            Self::Magenta => 5,
+            Self::Cyan    => 6,
+            Self::White   => 7,
+        }
+    }
+}
+
+
+/// Representation of brightness
+///
+#[derive(Copy, Clone)]
+enum Brightness {
+    Dark,
+    Light,
+}
+
+impl Brightness {
+    /// Determin the code offset corresponding to the brightness
+    ///
+    fn code_off(&self) -> u8 {
+        match self {
+            Self::Dark  =>  0,
+            Self::Light => 60,
+        }
+    }
+}
+

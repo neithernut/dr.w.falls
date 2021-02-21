b9c69f6590aa563dea5022e62753a7c8a369c0ee
Julian Ganz <neither@nut.email>
2021-02-21 19:22:51 +0100

    game: add types for game state updates and phase transitions

--- src/game.rs
+++ src/game.rs
@@ -4,3 +4,19 @@ mod lobby;
 mod waiting;
 mod round;
 
+
+/// Item type for game update channels
+///
+enum GameUpdate<U,T> {
+    Update(U),
+    PhaseEnd(PhaseEnd<T>),
+}
+
+
+/// Phase end messages
+///
+enum PhaseEnd<T> {
+    Transition(T),
+    EndOfGame,
+}
+

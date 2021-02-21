db9425776b7e86b2a51923b810f8119d8559d3ef
Julian Ganz <neither@nut.email>
2021-02-21 19:24:21 +0100

    game: add types for player handle and tag

--- src/game.rs
+++ src/game.rs
@@ -30,3 +30,41 @@ enum GamePhase {
     EndOfGame,
 }
 
+
+/// Player handle
+///
+#[derive(Default)]
+struct PlayerHandle {
+    data: std::sync::Arc<()>,
+}
+
+impl PlayerHandle {
+    /// Generate a tag for this player handle
+    ///
+    fn tag(&self) -> PlayerTag {
+        PlayerTag {data: std::sync::Arc::downgrade(&self.data)}
+    }
+}
+
+
+/// Player tag
+///
+/// A value of this type allows identifying a player (via comparison)
+///
+#[derive(Clone)]
+pub struct PlayerTag {
+    data: std::sync::Weak<()>,
+}
+
+impl PlayerTag {
+    pub fn is_alive(&self) -> bool {
+        self.data.strong_count() > 0
+    }
+}
+
+impl PartialEq<PlayerTag> for PlayerTag {
+    fn eq(&self, other: &PlayerTag) -> bool {
+        self.data.ptr_eq(&other.data)
+    }
+}
+

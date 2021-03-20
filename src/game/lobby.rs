8b3914700f977e5fed9f6c6accc448afe7ec9878
Julian Ganz <neither@nut.email>
2021-03-20 13:11:38 +0100

    game: add type for connection tokens

--- src/game/lobby.rs
+++ src/game/lobby.rs
@@ -1,2 +1,39 @@
 //! Implementation of the lobby phase
 
+use std::net::SocketAddr;
+use std::sync::Arc;
+
+
+/// Connection token
+///
+#[derive(Clone, Debug)]
+struct ConnectionToken {
+    data: Arc<SocketAddr>,
+}
+
+impl From<SocketAddr> for ConnectionToken {
+    fn from(addr: SocketAddr) -> Self {
+        Self {data: Arc::new(addr)}
+    }
+}
+
+impl Eq for ConnectionToken {}
+
+impl PartialEq for ConnectionToken {
+    fn eq(&self, other: &ConnectionToken) -> bool {
+        Arc::ptr_eq(&self.data, &other.data)
+    }
+}
+
+impl std::hash::Hash for ConnectionToken {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        Arc::as_ptr(&self.data).hash(state)
+    }
+}
+
+impl AsRef<SocketAddr> for ConnectionToken {
+    fn as_ref(&self) -> &SocketAddr {
+        &self.data
+    }
+}
+

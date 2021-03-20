2ff5311846c26da8ca52b7f42b7de9139e3366b1
Julian Ganz <neither@nut.email>
2021-03-20 13:13:12 +0100

    game: add types for registration requests and replies

--- src/game/lobby.rs
+++ src/game/lobby.rs
@@ -1,7 +1,79 @@
 //! Implementation of the lobby phase
 
+use std::fmt;
 use std::net::SocketAddr;
 use std::sync::Arc;
+
+use tokio::sync::oneshot;
+
+use crate::player;
+
+
+/// Registration request
+///
+#[derive(Debug)]
+struct Registration {
+    name: String,
+    token: ConnectionToken,
+    response: oneshot::Sender<RegistrationReply>,
+}
+
+impl Registration {
+    /// Create a new Registration
+    ///
+    pub fn new(
+        name: String,
+        token: ConnectionToken,
+        response: oneshot::Sender<RegistrationReply>
+    ) -> Self {
+        Self {name, token, response}
+    }
+}
+
+
+/// Reply to a registration request
+///
+#[derive(Debug)]
+enum RegistrationReply {
+    Accepted(player::Handle),
+    Denied(DenialReason),
+}
+
+impl From<player::Handle> for RegistrationReply {
+    fn from(handle: player::Handle) -> Self {
+        Self::Accepted(handle)
+    }
+}
+
+impl From<DenialReason> for RegistrationReply {
+    fn from(reason: DenialReason) -> Self {
+        Self::Denied(reason)
+    }
+}
+
+
+/// Reason for denial of a registration
+///
+#[derive(Copy, Clone, Debug)]
+enum DenialReason {
+    AcceptanceClosed,
+    MaxPlayers,
+    NameTaken,
+    TemporaryFailure,
+    PermanentFailure,
+}
+
+impl fmt::Display for DenialReason {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::AcceptanceClosed  => write!(f, "Registration is closed"),
+            Self::MaxPlayers        => write!(f, "Maximum number of players reached"),
+            Self::NameTaken         => write!(f, "Name is already taken"),
+            Self::TemporaryFailure  => write!(f, "Temporary registration failure"),
+            Self::PermanentFailure  => write!(f, "Permanent registration failure"),
+        }
+    }
+}
 
 
 /// Connection token

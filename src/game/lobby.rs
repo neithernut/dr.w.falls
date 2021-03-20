8e60a892282892c772bb86eb9952b7840397d0d7
Julian Ganz <neither@nut.email>
2021-03-20 13:02:18 +0100

    game: add ScoreBoardEntry for the lobby phase

--- src/game/lobby.rs
+++ src/game/lobby.rs
@@ -1,2 +1,38 @@
 //! Implementation of the lobby phase
 
+use crate::display;
+
+
+/// Score board entry for the lobby phase
+///
+struct ScoreBoardEntry {
+    name: String,
+    tag: super::PlayerTag,
+}
+
+impl display::ScoreBoardEntry for ScoreBoardEntry {
+    type Tag = super::PlayerTag;
+
+    type Extra = &'static str;
+
+    fn name(&self) -> &str {
+        self.name.as_ref()
+    }
+
+    fn tag(&self) -> Self::Tag {
+        self.tag.clone()
+    }
+
+    fn score(&self) -> u32 {
+        0
+    }
+
+    fn extra(&self) -> Self::Extra {
+        ""
+    }
+
+    fn active(&self) -> bool {
+        self.tag.is_alive()
+    }
+}
+

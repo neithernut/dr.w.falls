1540c80185914a977a05253a36a5bfeb411b6864
Julian Ganz <neither@nut.email>
2021-03-20 13:11:38 +0100

    game: add type for connection tokens

--- src/game/lobby.rs
+++ src/game/lobby.rs
@@ -1,6 +1,30 @@
 //! Implementation of the lobby phase
 
+use std::sync::Arc;
+
 use crate::display;
+
+
+/// Connection token
+///
+#[derive(Clone)]
+struct ConnectionToken {
+    data: Arc<()>,
+}
+
+impl Eq for ConnectionToken {}
+
+impl PartialEq for ConnectionToken {
+    fn eq(&self, other: &ConnectionToken) -> bool {
+        Arc::ptr_eq(&self.data, &other.data)
+    }
+}
+
+impl std::hash::Hash for ConnectionToken {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        Arc::as_ptr(&self.data).hash(state)
+    }
+}
 
 
 /// Score board entry for the lobby phase

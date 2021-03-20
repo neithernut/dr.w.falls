ddac5008ccb14f0e125262d410dd23a60f5f3a00
Julian Ganz <neither@nut.email>
2021-03-20 12:58:22 +0100

    main: add representation of player roster

--- src/main.rs
+++ src/main.rs
@@ -10,3 +10,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+
+/// Roster of players
+///
+type Roster = Vec<Player>;
+
+
+/// Representation of a single player within the roster
+///
+struct Player {
+    name: String,
+    tag: game::PlayerTag,
+    score: u32,
+}
+
+impl Player {
+    /// Create a new player
+    ///
+    pub fn new(name: String, tag: game::PlayerTag) -> Self {
+        Self {name, tag, score: 0}
+    }
+}
+

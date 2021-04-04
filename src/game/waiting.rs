06cb6cbe8208aa9c9d6f06718ab2c073222f2eed
Julian Ganz <neither@nut.email>
2021-04-04 22:10:18 +0200

    game: add waiting phase function

--- src/game/waiting.rs
+++ src/game/waiting.rs
@@ -1,9 +1,81 @@
 //! Implementation of the waiting phase
 
-use tokio::sync::{mpsc, watch};
+use tokio::io;
+use tokio::sync::{watch, mpsc};
 
 use crate::display;
 use crate::player;
+
+
+/// Waiting phase function
+///
+/// This function implements the connection task part of the game logic for the
+/// waiting phase.
+///
+async fn serve<P>(
+    control: Ports,
+    display: &mut display::Display<impl io::AsyncWrite + Unpin>,
+    mut input: impl futures::stream::Stream<Item = Result<char, io::Error>> + Unpin,
+    mut phase: super::TransitionWatcher<P, impl Fn(&P) -> bool>,
+    me: &player::Handle,
+) -> Result<(), super::ConnTaskError> {
+    use std::convert::TryInto;
+
+    use futures::stream::StreamExt;
+
+    use super::ConnTaskError;
+
+    let mut scores = control.scores;
+    let mut countdown = control.countdown;
+    let ready = control.ready;
+
+    // Set up the display
+    let mut area = display.area().await?.pad_top(1);
+    let mut left = area.split_left(super::COLUMN_SPLIT);
+    let mut ct = left.split_top(super::INSTRUCTION_SPLIT);
+
+    ct.place_top(display::StaticText::from("Round starts in:")).await?;
+    ct = ct.pad_top(1);
+    let num_display = ct.place_top(display::DynamicText::new_line(4u16.try_into().unwrap())).await?;
+    ct = ct.pad_top(1);
+    ct.place_top(display::StaticText::from("or when everybody's ready.")).await?;
+    ct = ct.pad_top(1);
+    let inst = ct.place_center(
+        display::DynamicText::new_line((super::COLUMN_SPLIT - 2).try_into().unwrap())
+    ).await?;
+
+    left.place_center(display::StaticText::from(super::INSTRUCTIONS.iter().cloned())).await?;
+
+    let max_scores = area.rows().saturating_sub(2);
+    let mut score_board = area.place_center(display::ScoreBoard::new(max_scores).show_scores(false)).await?;
+    score_board.update(&mut display.handle().await?, scores.borrow().iter(), |_| false).await?;
+
+
+    num_display.update_single(&mut display.handle().await?, *countdown.borrow()).await?;
+    inst.update_single(&mut display.handle().await?, "Press any key when ready.").await?;
+
+    // Actual waiting display logic
+    loop {
+        tokio::select! {
+            res = input.next() => match res {
+                Some(Ok(_)) => {
+                    ready.send(me.tag()).await.map_err(ConnTaskError::other)?;
+                    inst.update_single(&mut display.handle().await?, "Wait to the round to start.").await?;
+                },
+                Some(Err(e)) if e.kind() != io::ErrorKind::WouldBlock => return Err(e.into()),
+                None => return Err(ConnTaskError::Terminated),
+                _ => (),
+            },
+            _ = scores.changed() => score_board
+                .update(&mut display.handle().await?, scores.borrow().iter(), |_| false)
+                .await?,
+            _ = countdown.changed() => num_display
+                .update_single(&mut display.handle().await?, *countdown.borrow())
+                .await?,
+            t = phase.transition() => break t,
+        }
+    }
+}
 
 
 /// Create ports for communication between connection and control task

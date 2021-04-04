b9cf967749a8139048909efad7795d11d0a98761
Julian Ganz <neither@nut.email>
2021-04-04 21:03:03 +0200

    game: add lobby phase function

--- src/game/lobby.rs
+++ src/game/lobby.rs
@@ -2,9 +2,97 @@
 
 use std::sync::Arc;
 
-use tokio::sync::oneshot;
+use tokio::io;
+use tokio::sync::{watch, mpsc, oneshot};
 
 use crate::display;
+
+
+/// Lobby phase function
+///
+/// This function provides the connection-task side of the lobby phase logic.
+///
+async fn lobby<S, R>(
+    input: &mut super::ASCIIStream<'_>,
+    display: &mut super::Display<'_>,
+    updates: &mut watch::Receiver<GameUpdate<S, R>>,
+    reg_channel: mpsc::Sender<Registration>,
+    token: ConnectionToken,
+) -> io::Result<super::PlayerHandle> {
+    use futures::stream::StreamExt;
+
+    // Set up display
+    let (left, right) = super::columns(display);
+    let (mut text, left) = left.top_in("Enter name:");
+    let (mut name_display, left) = left
+        .top_padded(1)
+        .top_in(display::LineInputFactory::new(NAME_MAX_LEN));
+    let mut scoreboard: display::ScoreBoard<ScoreBoardEntry> = right
+        .topleft_in(display::ScoreBoardFactory::default());
+
+    text.draw(display).await?;
+    scoreboard.render_heading(display, "").await?;
+
+    // Get the player to register
+    let mut name: String = Default::default();
+    let player: super::PlayerHandle = loop {
+        tokio::select! {
+            res = input.next() => match res {
+                Some(Ok('\x03')) | Some(Ok('\x04')) => return Err(io::ErrorKind::UnexpectedEof.into()),
+                Some(Ok('\x0A')) | Some(Ok('\x0D')) => {
+                    // "Enter": try to perform registration
+                    let (response_send, response) = oneshot::channel();
+                    reg_channel
+                        .send(Registration::new(name.clone(), token.clone(), response_send))
+                        .await
+                        .map_err(|_| io::Error::from(io::ErrorKind::Other))?;
+                    match response.await.map_err(|_| io::Error::from(io::ErrorKind::Other))? {
+                        RegistrationReply::Accepted(handle) => break handle,
+                        RegistrationReply::Denied(_) => (), // TODO: handle reason
+                    }
+                }
+                Some(Ok('\x08')) => {
+                    // Backspace: remove the last char from the name
+                    name.pop();
+                    name_display.update(display, name.as_ref()).await?;
+                }
+                Some(Ok(c)) => if !c.is_ascii_control() && name.len() < NAME_MAX_LEN as usize {
+                    name.push(c);
+                    name_display.update(display, name.as_ref()).await?;
+                },
+                Some(Err(e)) if e.kind() == io::ErrorKind::WouldBlock => (),
+                Some(Err(e)) => return Err(e),
+                None => (),
+            },
+            _ = updates.changed() => match &*updates.borrow() {
+                GameUpdate::Update(players) => scoreboard
+                    .update(display, players.clone(), &super::PlayerHandle::default().tag())
+                    .await?,
+                GameUpdate::PhaseEnd(_) => return Err(io::ErrorKind::Other.into()),
+            },
+        }
+    };
+
+    // Wait for game to start
+    left.top_padded(1).top_in("You are registred.\nAwaiting game.").0.draw(display).await?;
+    loop {
+        tokio::select! {
+            res = input.next() => match res {
+                Some(Ok('\x03')) | Some(Ok('\x04')) => return Err(io::ErrorKind::UnexpectedEof.into()),
+                Some(Err(e)) if e.kind() != io::ErrorKind::WouldBlock => return Err(e),
+                _ => (),
+            },
+            _ = updates.changed() => match &*updates.borrow() {
+                GameUpdate::Update(players) => scoreboard
+                    .update(display, players.clone(), &super::PlayerHandle::default().tag())
+                    .await?,
+                GameUpdate::PhaseEnd(_) => break,
+            },
+        }
+    }
+
+    Ok(player)
+}
 
 
 /// Local type for game updates

cd4440123863ecc38cbe27d51fe7e063400c2d51
Julian Ganz <neither@nut.email>
2021-04-04 21:30:12 +0200

    game: add ScoreBoardEntry for the waiting phase

--- src/game/waiting.rs
+++ src/game/waiting.rs
@@ -1,2 +1,44 @@
 //! Implementation of the waiting phase
 
+use crate::display;
+use crate::player;
+
+
+/// Score board entry for the waiting phase
+///
+#[derive(Clone, Debug)]
+struct ScoreBoardEntry {
+    tag: player::Tag,
+    ready: bool,
+}
+
+impl ScoreBoardEntry {
+    /// Mark the player as ready
+    ///
+    fn set_ready(&mut self) {
+        self.ready = true;
+    }
+
+    /// Retrieve the player's readyness
+    ///
+    fn ready(&self) -> bool {
+        self.ready
+    }
+}
+
+impl From<player::Tag> for ScoreBoardEntry {
+    fn from(tag: player::Tag) -> Self {
+        Self {tag, ready: false}
+    }
+}
+
+impl display::ScoreBoardEntry for ScoreBoardEntry {
+    fn tag(&self) -> &player::Tag {
+        &self.tag
+    }
+
+    fn active(&self) -> bool {
+        self.ready
+    }
+}
+

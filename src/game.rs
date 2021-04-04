0b3421e3233050215394f957638a15ddf0cbd3c4
Julian Ganz <neither@nut.email>
2021-04-04 20:44:41 +0200

    game: add an input stream for retrieving individual characters
    Using a stream will save us from some annoying buffer handling in the
    various phase functions as well as looking for ETX and EOT, although
    this is borderline abusing the `Decoder` interface.

--- src/game.rs
+++ src/game.rs
@@ -4,3 +4,42 @@ mod lobby;
 mod waiting;
 mod round;
 
+use tokio::io;
+
+
+/// A stream of ASCII characters
+///
+type ASCIIStream<'a, R> = tokio_util::codec::FramedRead<R, ASCIICharDecoder>;
+
+
+/// Decoder for single ASCII characters
+///
+/// This decoder yields (confirmed) ASCII characters. In addition, it emulates
+/// an enf-of-file condition on ETX (`0x03`) and EOT (`0x04`) by issuing an
+/// `UnexpectedEof` error.
+///
+struct ASCIICharDecoder {}
+
+impl tokio_util::codec::Decoder for ASCIICharDecoder {
+    type Item = char;
+    type Error = io::Error;
+
+    fn decode(
+        &mut self,
+        src: &mut bytes::BytesMut
+    ) -> Result<Option<Self::Item>, Self::Error> {
+        use bytes::Buf;
+
+        if src.has_remaining() {
+            match src.get_u8() {
+                0x03 | 0x04         => Err(io::ErrorKind::UnexpectedEof.into()),
+                c if c.is_ascii()   => Ok(Some(c as char)),
+                _                   => Err(io::ErrorKind::InvalidData.into())
+            }
+        } else {
+            src.reserve(1);
+            Ok(None)
+        }
+    }
+}
+

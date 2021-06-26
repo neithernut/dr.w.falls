38a7143764f194aa636be59711c69ec86de72cb7
Julian Ganz <neither@nut.email>
2021-06-26 20:55:56 +0200

    display: make types intended for use public

--- src/display.rs
+++ src/display.rs
@@ -9,3 +9,12 @@ mod input;
 mod scores;
 mod static_text;
 
+
+pub use area::Area;
+pub use display::Display;
+pub use dynamic_text::DynamicText;
+pub use field::PlayField;
+pub use input::LineInput;
+pub use scores::{Entry as ScoreBoardEntry, ScoreBoard};
+pub use static_text::StaticText;
+

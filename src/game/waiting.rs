46a2a83d7ae7167fe93eed58707d38da81a88035
Julian Ganz <neither@nut.email>
2021-07-04 17:18:46 +0200

    game: add types encapsulating channels for the waiting phase
    We want to hise phase specific internals from the outside. However, we
    do need to handle the channels in the toplevel `game` module. The design
    specification already mentions a type encapsulating the senders and
    receivers used by the connection task in an opaque struct. We decided to
    do the same for the control task.

--- src/game/waiting.rs
+++ src/game/waiting.rs
@@ -1,7 +1,46 @@
 //! Implementation of the waiting phase
 
+use tokio::sync::{mpsc, watch};
+
 use crate::display;
 use crate::player;
+
+
+/// Create ports for communication between connection and control task
+///
+/// This function returns a pair of ports specific to the waiting phase, one for
+/// the connection task and one for the control task.
+///
+pub fn ports() -> (Ports, ControlPorts) {
+    let (score_sender, score_receiver) = watch::channel(Default::default());
+    let (countdown_sender, countdown_receiver) = watch::channel(Default::default());
+    let (readiness_sender, readiness_receiver) = mpsc::channel(20); // TODO: replace hard-coded value?
+
+    let ports = Ports {scores: score_receiver, countdown: countdown_receiver, ready: readiness_sender};
+    let control = ControlPorts {scores: score_sender, countdown: countdown_sender, ready: readiness_receiver};
+
+    (ports, control)
+}
+
+
+/// Connection task side of communication ports for the lobby phase
+///
+#[derive(Clone, Debug)]
+pub struct Ports {
+    scores: watch::Receiver<Vec<ScoreBoardEntry>>,
+    countdown: watch::Receiver<u8>,
+    ready: mpsc::Sender<player::Tag>,
+}
+
+
+/// Control task side of communication ports for the lobby phase
+///
+#[derive(Debug)]
+pub struct ControlPorts {
+    scores: watch::Sender<Vec<ScoreBoardEntry>>,
+    countdown: watch::Sender<u8>,
+    ready: mpsc::Receiver<player::Tag>,
+}
 
 
 /// Score board entry for the waiting phase

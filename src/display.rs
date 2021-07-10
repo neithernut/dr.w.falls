e1a734901761ae42dc762917b8af89eaefa188bf
Julian Ganz <neither@nut.email>
2021-07-10 16:34:51 +0200

    display: make FieldUpdater public
    Unlike the other `PlacedEntities`, we will probably want to refer to
    this updater by name.

--- src/display.rs
+++ src/display.rs
@@ -14,7 +14,7 @@ pub use area::Area;
 pub use commands::DrawHandle;
 pub use display::Display;
 pub use dynamic_text::DynamicText;
-pub use field::PlayField;
+pub use field::{FieldUpdater, PlayField};
 pub use input::LineInput;
 pub use scores::{Entry as ScoreBoardEntry, ScoreBoard};
 pub use static_text::StaticText;

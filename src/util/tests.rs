26cf4de3ffefc6dfca6863a017c278f5645cd0ec
Julian Ganz <neither@nut.email>
2021-07-22 15:27:36 +0200

    util: add tests for COLUMNS and ROWS ranges

--- src/util/tests.rs
+++ src/util/tests.rs
@@ -3,6 +3,22 @@
 use super::*;
 
 
+#[test]
+fn rows_len() {
+    assert_eq!(ROWS.len(), FIELD_HEIGHT as usize);
+    assert_eq!(ROWS.count(), FIELD_HEIGHT as usize);
+    assert_eq!(ROWS.rfold(0, |c, _| c + 1), FIELD_HEIGHT as usize);
+}
+
+
+#[test]
+fn columns_len() {
+    assert_eq!(COLUMNS.len(), FIELD_WIDTH as usize);
+    assert_eq!(COLUMNS.count(), FIELD_WIDTH as usize);
+    assert_eq!(COLUMNS.rfold(0, |c, _| c + 1), FIELD_WIDTH as usize);
+}
+
+
 #[quickcheck]
 fn colour_rotation(colour: Colour, dir: bool) -> bool {
     let c1 = colour.rotate(dir);

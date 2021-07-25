162b4e1008afa793c49af8e7890e0a3dadc3ebd5
Julian Ganz <neither@nut.email>
2021-07-25 12:21:34 +0200

    field: use Arbitrary impl for util::RangeInclusive in impl for RowOfFour

--- src/field/items.rs
+++ src/field/items.rs
@@ -157,19 +157,11 @@ impl Iterator for RowOfFour {
 impl Arbitrary for RowOfFour {
     fn arbitrary(g: &mut Gen) -> Self {
         fn horizontal(g: &mut Gen) -> RowOfFour {
-            let a = Arbitrary::arbitrary(g);
-            let b = Arbitrary::arbitrary(g);
-            let min = std::cmp::min(a, b);
-            let max = std::cmp::max(a, b);
-            RowOfFour::Horizontal(Arbitrary::arbitrary(g), util::RangeInclusive::new(min, max))
+            RowOfFour::Horizontal(Arbitrary::arbitrary(g), Arbitrary::arbitrary(g))
         }
 
         fn vertical(g: &mut Gen) -> RowOfFour {
-            let a = Arbitrary::arbitrary(g);
-            let b = Arbitrary::arbitrary(g);
-            let min = std::cmp::min(a, b);
-            let max = std::cmp::max(a, b);
-            RowOfFour::Vertical(util::RangeInclusive::new(min, max), Arbitrary::arbitrary(g))
+            RowOfFour::Vertical(Arbitrary::arbitrary(g), Arbitrary::arbitrary(g))
         }
 
         let opts = [horizontal, vertical];
